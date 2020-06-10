5cbfab3378a6caf3612aca4c6c8471dba08f7e50
tanviredu <ornobtanvir.git@gmail.com>
2020-06-10 23:29:11 +0600

    added

--- class_attribute/shipping.py
+++ class_attribute/shipping.py
@@ -5,6 +5,17 @@ class ShippingContiner:
     
     next_serial = 1337
     
+    
+    @staticmethod
+    def _generate_serial():
+        
+        ## first return then increment the value
+        ## not return the increented value
+        
+        result = ShippingContiner.next_serial
+        ShippingContiner.next_serial +=1
+        return result 
+    
     ## this is the dunder init (connstructor method)
     def __init__(self,owner_code,contents):
         self.owner_code = owner_code
@@ -12,15 +23,14 @@ class ShippingContiner:
         ## class attribute shuld be called with the 
         ## after the clas method
         
-        self.serial = ShippingContiner.next_serial
-        ## increment
-        ## call the attribute with the Classname
-        ShippingContiner.next_serial+=1 
+        self.serial = ShippingContiner._generate_serial()
         
         
         
         
 def main():
+    ## inside the class the attribute value is tracked
+    ## so in gets incrememted
     
     c1 = ShippingContiner('YML',["books"])
     print(c1.next_serial)
